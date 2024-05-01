b07dcb4654c1821a94631cde15e69e2355187447
akosirukawa <168661158+akosirukawa@users.noreply.github.com>
2024-05-01 23:53:50 +0800

    Add files via upload

---
+++ bagongpayl.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+
+with st.container():
+ st.subheader("Hi, I am Hacker :wave:")
+ st.title("A data analysis from philippines")
+ st.write("I dont give a fuck")
+ st.write("[Free Cosmetics >](https://www.youtube.com/watch?v=xvFZjo5PgG0)")
